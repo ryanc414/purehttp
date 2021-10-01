c2c59b5f27dc2492cfe7da9e5362c58290b6c828
Ryan Collingham <ryan@tokencard.io>
2021-10-01 21:43:22 +0100

    implement happy-path logic

--- server.go
+++ server.go
@@ -2,7 +2,11 @@ package purehttp
 
 import "net/http"
 
-type HTTPResponse struct{}
+type HTTPResponse struct {
+	Body       []byte
+	StatusCode int
+	Header     http.Header
+}
 
 type HandlerFunc func(req *http.Request) (*HTTPResponse, error)
 
@@ -15,8 +19,26 @@ func NewHandler(handle HandlerFunc) Handler {
 }
 
 func (h Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	_, err := h.handle(req)
+	rsp, err := h.handle(req)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	header := w.Header()
+
+	for h := range rsp.Header {
+		val := rsp.Header.Get(h)
+		header.Set(h, val)
+	}
+
+	statusCode := rsp.StatusCode
+	if statusCode == 0 {
+		statusCode = http.StatusOK
+	}
+
+	w.WriteHeader(statusCode)
+	if len(rsp.Body) > 0 {
+		w.Write(rsp.Body)
 	}
 }
--- server_test.go
+++ server_test.go
@@ -1,7 +1,9 @@
 package purehttp_test
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,7 +13,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestServer(t *testing.T) {
+func TestError(t *testing.T) {
 	f := func(req *http.Request) (*purehttp.HTTPResponse, error) {
 		return nil, errors.New("not implemented")
 	}
@@ -24,4 +26,59 @@ func TestServer(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, http.StatusInternalServerError, rsp.StatusCode)
+	data, err := ioutil.ReadAll(rsp.Body)
+	rsp.Body.Close()
+	require.NoError(t, err)
+
+	assert.Equal(t, "not implemented\n", string(data))
+}
+
+func TestHappy(t *testing.T) {
+	f := func(req *http.Request) (*purehttp.HTTPResponse, error) {
+		return &purehttp.HTTPResponse{
+			Body:       []byte("{\"foo\":\"bar\"}"),
+			StatusCode: http.StatusAccepted,
+			Header:     http.Header{"Content-Type": []string{"application/json"}},
+		}, nil
+	}
+
+	h := purehttp.NewHandler(f)
+	ts := httptest.NewServer(h)
+	t.Cleanup(ts.Close)
+
+	rsp, err := http.Get(ts.URL)
+	require.NoError(t, err)
+
+	assert.Equal(t, "application/json", rsp.Header.Get("Content-Type"))
+
+	assert.Equal(t, http.StatusAccepted, rsp.StatusCode)
+	data, err := ioutil.ReadAll(rsp.Body)
+	rsp.Body.Close()
+	require.NoError(t, err)
+
+	var res struct{ Foo string }
+	err = json.Unmarshal(data, &res)
+	require.NoError(t, err)
+
+	assert.Equal(t, "bar", res.Foo)
+}
+
+func TestDefault(t *testing.T) {
+	f := func(req *http.Request) (*purehttp.HTTPResponse, error) {
+		return &purehttp.HTTPResponse{}, nil
+	}
+
+	h := purehttp.NewHandler(f)
+	ts := httptest.NewServer(h)
+	t.Cleanup(ts.Close)
+
+	rsp, err := http.Get(ts.URL)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, rsp.StatusCode)
+	data, err := ioutil.ReadAll(rsp.Body)
+	rsp.Body.Close()
+	require.NoError(t, err)
+
+	assert.Empty(t, data)
 }
